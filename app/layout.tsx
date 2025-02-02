c153a35830abc533047f73b0545c510b42117078
Blackxok <nasridoni1@gmail.com>
2025-02-02 12:14:50 +0500

    AuthContext indexPage som enimpuvment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,21 @@
 'use client'
-import Navbar from '@/components/navbar/navbar'
-import { useEffect, useState } from 'react'
+
+import { AuthProvider } from '@/context/AuthContext'
+import Head from 'next/head'
 import '../styles/globals.css'
-import Sidebar from './(root)/(home)/_components/Sidebar'
-import LoginPage from './login/page'
+import IndexPage from './page'
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode
-}>) {
-	const [isAuth, setIsAuth] = useState<boolean>(false)
-	console.log(isAuth)
-
-	useEffect(() => {
-		const token = localStorage.getItem('authToken')
-		if (token) {
-			setIsAuth(true) 
-		}
-	}, [])
-
-	// const isAuth = localStorage.getItem('authToken')
-
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang='en'>
-			<body className='antialiased'>
-				{isAuth ? (
-					<div className='flex flex-col h-screen'>
-						<Navbar />
-						<div className='flex flex-col md:flex-row flex-1 px-2 md:px-[10px]'>
-							<Sidebar />
-							<main className='flex-1 overflow-y-auto p-6'>{children}</main>
-						</div>
-					</div>
-				) : (
-					<LoginPage setIsAuth={setIsAuth} />
-				)}
+			<Head>
+				<title>My App</title>
+				<meta name='description' content='An authentication app using Next.js' />
+			</Head>
+			<body className='antialiased min-h-screen'>
+				<AuthProvider>
+					<IndexPage>{children}</IndexPage>
+				</AuthProvider>
 			</body>
 		</html>
 	)
